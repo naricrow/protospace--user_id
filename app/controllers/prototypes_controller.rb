9231bece52f2dd94d83726677ca415fe22846cfd
naricrow <nari.crow.city@gmail.com>
2023-10-18 06:52:14 +0900

    デプロイ

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,12 +1,13 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit ,:destroy]
-  before_action :move_to_index, except: :edit
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user).all
   end
 
   def new
-    @prototype =  Prototype.new
+    @prototype = Prototype.new
   end
 
   def create
@@ -19,36 +20,25 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
-      redirect_to root_path
+      redirect_to prototype_path
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
-
-  def show
-    @prototype = Prototype.find(params[:id])
-    @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
-  end
-
-
 
   private
 
@@ -56,9 +46,14 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user == current_user
+      redirect_to root_path
     end
   end
 end
